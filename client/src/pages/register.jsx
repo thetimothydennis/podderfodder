048409ae4ac1f62a92128915f0350a8d8f885b88
Timothy Dennis <timothyddennis@gmail.com>
2023-07-20 14:42:27 -0500

    feat: created frontend pages for login and register

---
+++ client/src/pages/register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function UpdatePod () {
+    return (
+        <div className="Epi">
+            <form method="POST" action="/api/register">
+                <label htmlFor="username">username</label>
+                <input type="text" name="username" id="username" />
+                <label htmlFor="password">password</label>
+                <input type="password" name="password" id="password" />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
