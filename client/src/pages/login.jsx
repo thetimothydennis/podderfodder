d2914bc7c2912903d866269e5ba791668ea20ed7
Timothy Dennis <timothyddennis@gmail.com>
2023-08-03 17:38:19 -0500

    feat: changed social login buttons to images

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -14,8 +14,10 @@ export function UserLogin () {
                 </p>
                 <button type="submit" className="btn btn-dark">Login</button>
                 <p>Or, you can login with one of these social providers:</p>
-                <a type="btn" className="btn btn-secondary" href="/github">Login with Github</a>
-                <a type="btn" className="btn btn-secondary" href="/google">Login with Google</a>
+                <div className="row">
+                    <a className="col-sm" href="/github"><img height="45px" alt="login with github" src="/buttons/github_normal.png" /></a>
+                    <a className="col-sm" href="/google"><img height="45px" alt="login with google" src="/buttons/google_normal.png" /></a>
+                </div>
             </form>
             <p>Don&apos;t have an account yet? Create one <a href="/register">here</a>.</p>
             <p>Forgot your password? Click <a href="/forgotpassword">here</a>.</p>
