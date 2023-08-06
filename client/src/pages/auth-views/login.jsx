f8ed8397cd07c11dcc3209fb140c36a1ffec9895
Timothy Dennis <timothyddennis@gmail.com>
2023-08-06 09:41:55 -0500

    fix: added pointer events to login buttons

--- client/src/pages/auth-views/login.jsx
+++ client/src/pages/auth-views/login.jsx
@@ -12,8 +12,16 @@ export function UserLogin () {
         setGitSrc('/buttons/github_pressed.png');
     };
 
+    const handleGHReset = () => {
+        setGitSrc('/buttons/github_normal.png');
+    };
+
     const handleGoogClick = () => {
         setGoogSrc('/buttons/google_pressed.png');
+    };
+
+    const handleGoogReset = () => {
+        setGoogSrc('/buttons/google_normal.png');
     };
 
     return (
@@ -22,23 +30,51 @@ export function UserLogin () {
             <p>You must log in before accessing the platform.</p>
             <form className="container" method="POST" action="/api/login">
                 <p className="row form-group">
-                    <label className="col-sm form-control" htmlFor="username">username
-                    <input className="col-sm form-control" type="text" name="username" id="username" /></label>
+                    <label className="col-sm form-control" 
+                           htmlFor="username">username
+                    <input className="col-sm form-control" 
+                           type="text" 
+                           name="username" 
+                           id="username" /></label>
                 </p>
                 <p className="row form-group">
-                    <label className="col-sm form-control" htmlFor="password">password
-                    <input className="col-sm form-control" type="password" name="password" id="password" /></label>
+                    <label className="col-sm form-control" 
+                           htmlFor="password">password
+                    <input className="col-sm form-control" 
+                           type="password" 
+                           name="password" 
+                           id="password" /></label>
                 </p>
                 <button type="submit" className="btn btn-dark">Login</button>
                 <p>Don&apos;t have an account yet? Create one <a href="/register">here</a>.</p>
                 <p>Forgot your password? Click <a href="/forgotpassword">here</a>.</p>
                 <p>Or, you can login with one of these social providers:</p>
                 <div className="row">
-                    <a className="col-sm" href="/github" style={{"pointer-events": "all"}} onClick={handleGHClick}>
-                        <img id="github" height="45px" alt="login with github" src={gitSrc} />
+                    <a className="col-sm" 
+                       href="/github" 
+                       style={{"pointer-events": "all"}} 
+                       onPointerEnter={handleGHClick} 
+                       onPointerDown={handleGHClick} 
+                       onPointerLeave={handleGHReset} 
+                       onPointerUp={handleGHReset} 
+                       onClick={handleGHClick}>
+                        <img id="github" 
+                             height="45px" 
+                             alt="login with github" 
+                             src={gitSrc} />
                     </a>
-                    <a className="col-sm" href="/google" style={{"pointer-events": "all"}} onClick={handleGoogClick}>
-                        <img id="google" height="45px" alt="login with google" src={googSrc} />
+                    <a className="col-sm" 
+                       href="/google" 
+                       style={{"pointer-events": "all"}} 
+                       onPointerEnter={handleGoogClick} 
+                       onPointerDown={handleGoogClick} 
+                       onPointerLeave={handleGoogReset} 
+                       onPointerUp={handleGoogReset} 
+                       onClick={handleGoogClick}>
+                        <img id="google" 
+                             height="45px" 
+                             alt="login with google" 
+                             src={googSrc} />
                     </a>
                 </div>
                 <p className="note">NOTE: If you&apos;ve already used a Google email address to sign up, you will not be able to sign in with Google using that account.</p>
