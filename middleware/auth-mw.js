9932fd1478cae9013a2c5cc0a76a9808ea9db172
Timothy Dennis <timothyddennis@gmail.com>
2023-08-25 00:01:58 -0500

    fix: made socket.io middleware more fault tolderant, adjusted css to have more uniform input fields across pages

--- middleware/auth-mw.js
+++ middleware/auth-mw.js
@@ -2,16 +2,20 @@ import { Socket } from "../config/sockets.js";
 
 export const socketMW = (req, res, next) => {
 	let room = req.sessionID;
-	let { messages } = req.session;
-	if (messages !== undefined && messages.length > 0) {
-		for (let message of messages) {
-			Socket.emit("error", {
-					message: message,
-				}, room);
-		};
-		req.session.messages = [];
-		next();
+	if (room) {
+		let { messages } = req.session;
+		if (messages !== undefined && messages.length > 0) {
+			for (let message of messages) {
+				Socket.emit("error", {
+						message: message,
+					}, room);
+			};
+			req.session.messages = [];
+			next();
+		} else {
+			next();
+		};	
 	} else {
 		next();
-	};
+	}
 };
